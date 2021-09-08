dfcf7c37c88f2ebd2e7ee1b4a897d3cb076645fb
yaninalex2003 <yanin.alex2003@yandex.ru>
2021-09-08 12:20:41 +0300

    I rewrote the code, I separated out the individual functions. So far, the answer is not completely correct

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,54 +1,71 @@
 import java.io.File
-fun max(a:Int, b:Int):Int{
-    if (a>b) return a
-    else return b
-}
+
 fun main(args: Array<String>) {
-    var text1: Array<String> = arrayOf()
-    for (line in File("test1.txt").readLines()){
-        text1=text1+line
-    }
-    var text2: Array<String> = arrayOf()
-    for (line in File("test2.txt").readLines()) {
-        text2 = text2 + line
-    }
-    val shirina=text1.size
-    val vysota=text2.size
-    var matrix: Array<Array<Int>> = Array(vysota+1, {Array( shirina+1, {0})})
-    for (i in 1..vysota){
-        for (j in 1..shirina){
-            if (text1[j-1]==text2[i-1]){
-                matrix[j][i]=matrix[j-1][i-1]+1
+    var text1 = scanFile("test1.txt")
+    var text2 = scanFile("test2.txt")
+    val ourMatrix = matrix(text1, text2)
+    val ourLCS = findLCS(ourMatrix, text1, text2)
+    printAns(text1, text2, ourLCS)
+}
+
+//нахождения максимума из двух чисел
+fun max(a: Int, b: Int): Int {
+    return if (a > b) a
+    else b
+}
+
+//считывание файла
+fun scanFile(name: String): Array<String> {
+    var text: Array<String> = arrayOf()
+    for (line in File(name).readLines())
+        text += line
+    return text
+}
+
+//построение матрицы для алгоритма поиска LCS
+fun matrix(text1: Array<String>, text2: Array<String>): Array<Array<Int>> {
+    var ourMatrix: Array<Array<Int>> = Array(text2.size + 1) { Array(text1.size + 1) { 0 } }
+    for (i in 1..text2.size) {
+        for (j in 1..text1.size) {
+            if (text1[j - 1] == text2[i - 1]) {
+                ourMatrix[i][j] = ourMatrix[i - 1][j - 1] + 1
+            } else {
+                ourMatrix[i][j] = max(ourMatrix[i][j - 1], ourMatrix[i - 1][j])
             }
-            else{
-                matrix[j][i]=max(matrix[j][i-1],matrix[j-1][i])
-            }
         }
     }
-    var ans: Array<String> = arrayOf()
-    var i = vysota
-    var j = shirina
-    while (i>0 && j>0) {
-        if (text1[j-1]==text2[j-1]){
-            ans = ans +text1[j-1]
-            i-=1
-            j-=1
-        }
-        else if (matrix[j][i]==matrix[j-1][i]) {
-            j-=1
-        }
-        else{
-            i-=1
+    return ourMatrix
+}
+
+//Восстановление LCS по матрице
+fun findLCS(ourMatrix: Array<Array<Int>>, text1: Array<String>, text2: Array<String>): Array<String> {
+    var ourLCS: Array<String> = arrayOf()
+    var i = text2.size
+    var j = text1.size
+    while (i > 0 && j > 0) {
+        if (text1[j - 1] == text2[i - 1]) {
+            ourLCS += text1[j - 1]
+            i -= 1
+            j -= 1
+        } else if (ourMatrix[i][j] == ourMatrix[i - 1][j]) {
+            i -= 1
+        } else {
+            j -= 1
         }
     }
-    var a = max(vysota-1, shirina-1)
-    while (a>=0){
-        if (a<=shirina-1 && text1[a] !in ans) {
-            println("Удалить строку номер ${a+1} : ${text1[a]}")
+    return ourLCS
+}
+
+//Вывод ответа
+fun printAns(text1: Array<String>, text2: Array<String>, ourLCS: Array<String>) {
+    var a = max(text2.size - 1, text1.size - 1)
+    while (a >= 0) {
+        if (a <= text1.size - 1 && text1[a] !in ourLCS) {
+            println("Удалить строку номер ${a + 1} : ${text1[a]}")
         }
-        if (a<=vysota-1 && text2[a] !in ans) {
-            println("Добавить строку номер ${a+1} : ${text2[a]}")
+        if (a <= text2.size - 1 && text2[a] !in ourLCS) {
+            println("Добавить строку номер ${a + 1} : ${text2[a]}")
         }
-        a-=1
+        a -= 1
     }
 }
