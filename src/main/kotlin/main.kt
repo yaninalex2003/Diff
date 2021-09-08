ef657b924123cbee14ce057af0eaa3a33d0c780a
yaninalex2003 <yanin.alex2003@yandex.ru>
2021-09-08 18:33:18 +0300

    I wrote correct code, that passes my tests

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,12 @@
 import java.io.File
 
 fun main(args: Array<String>) {
-    var text1 = scanFile("test1.txt")
-    var text2 = scanFile("test2.txt")
+    val text1 = scanFile("test1.txt")
+    val text2 = scanFile("test2.txt")
     val ourMatrix = matrix(text1, text2)
-    val ourLCS = findLCS(ourMatrix, text1, text2)
-    printAns(text1, text2, ourLCS)
+    val ourLCS1 = findLCS(ourMatrix, text1, text2)[0]
+    val ourLCS2 = findLCS(ourMatrix, text1, text2)[1]
+    printAns(text1, text2, ourLCS1, ourLCS2)
 }
 
 //нахождения максимума из двух чисел
@@ -38,13 +39,15 @@ fun matrix(text1: Array<String>, text2: Array<String>): Array<Array<Int>> {
 }
 
 //Восстановление LCS по матрице
-fun findLCS(ourMatrix: Array<Array<Int>>, text1: Array<String>, text2: Array<String>): Array<String> {
-    var ourLCS: Array<String> = arrayOf()
+fun findLCS(ourMatrix: Array<Array<Int>>, text1: Array<String>, text2: Array<String>): Array<Array<Int>> {
+    var ourLCS1: Array<Int> = arrayOf()
+    var ourLCS2: Array<Int> = arrayOf()
     var i = text2.size
     var j = text1.size
     while (i > 0 && j > 0) {
         if (text1[j - 1] == text2[i - 1]) {
-            ourLCS += text1[j - 1]
+            ourLCS2 += i - 1
+            ourLCS1 += j - 1
             i -= 1
             j -= 1
         } else if (ourMatrix[i][j] == ourMatrix[i - 1][j]) {
@@ -53,19 +56,24 @@ fun findLCS(ourMatrix: Array<Array<Int>>, text1: Array<String>, text2: Array<Str
             j -= 1
         }
     }
-    return ourLCS
+    return arrayOf(ourLCS1, ourLCS2)
 }
 
 //Вывод ответа
-fun printAns(text1: Array<String>, text2: Array<String>, ourLCS: Array<String>) {
-    var a = max(text2.size - 1, text1.size - 1)
-    while (a >= 0) {
-        if (a <= text1.size - 1 && text1[a] !in ourLCS) {
-            println("Удалить строку номер ${a + 1} : ${text1[a]}")
+fun printAns(text1: Array<String>, text2: Array<String>, ourLCS1: Array<Int>, ourLCS2: Array<Int>) {
+    var indexText1 = text1.size - 1
+    var indexText2 = text2.size - 1
+    for (i in 0..ourLCS1.size){
+        while (indexText1 !in ourLCS1 && indexText1>=0) {
+            println("Удалить строку с места номер ${indexText1+1}: ${text1[indexText1]}")
+            indexText1-=1
         }
-        if (a <= text2.size - 1 && text2[a] !in ourLCS) {
-            println("Добавить строку номер ${a + 1} : ${text2[a]}")
+        val place = indexText1+2
+        while (indexText2 !in ourLCS2 && indexText2>=0) {
+            println("Добавить строку на место номер $place: ${text2[indexText2]}")
+            indexText2-=1
         }
-        a -= 1
+        indexText2-=1
+        indexText1-=1
     }
 }
